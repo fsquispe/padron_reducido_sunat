4448c48c73f7668ec9a03efaa870bb80668e16d6
Fredy Quispe <fredysqm@gmail.com>
2018-03-20 08:26:14 -0500

    beta2

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
         db="sunat"
     )
     
-    group_insert = 10000
+    group_insert = 1000
     
     query_base = """
         INSERT INTO padron (
@@ -41,21 +41,21 @@ def main():
     query_params = "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"
     
     i = 0
+    n = 0
     query_block = ''
     with open(filename, 'r', encoding='ISO-8859-1') as f:
         for line in tqdm(f, total=num_records):
             i += 1
+            n += 1
             l = line.replace("\\", "")
             l = l.replace("'", "\\'")
             lst = l.split('|')
             query_block += (query_params % (lst[0], lst[1], lst[2], lst[3], lst[4], lst[5], lst[6], lst[7], lst[8], lst[9], lst[10], lst[11], lst[12], lst[13], lst[14]))
-            if i < group_insert: query_block += ','
-            if i == group_insert:
+            if i < group_insert and n < num_records: query_block += ','
+            if i == group_insert or n == num_records:
                 db.query(query_base % query_block)
                 i = 0
                 query_block = ''
-                
-
 
 if __name__ == "__main__":
 	main()
